cf97b9c1d9f9fd54c1ec652a3f89b919088d3fc5
rodrigo.sipriano@yaman.com.br+1-at-401235693475 <rodrigo.sipriano@yaman.com.br+1-at-401235693475>
2018-11-26 02:04:37 -0200

    First commit

---
+++ src/main/java/com/rodrigo/sipriano/PersonalWebsite/PersonalWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.rodrigo.sipriano.PersonalWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWebsiteApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rodrigo/sipriano/PersonalWebsite/PersonalWebsiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodrigo.sipriano.PersonalWebsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonalWebsiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
